50dcb9dfd72ff7357775e20c00a22e516ae27fd4
data-pup <kevinmartinucr@gmail.com>
2018-03-19 18:52:04 -0700

    Implemented a basic infinite sequence counter.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct InfiniteSeq {
+    curr: u32,
+}
+
+impl Iterator for InfiniteSeq {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let ret_value = self.curr;
+        self.curr = self.curr + 1;
+        Some(ret_value)
+    }
+}
+
+fn get_counter() -> InfiniteSeq {
+    InfiniteSeq { curr: 0 }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let counter = get_counter();
+
+    for i in counter.take_while(|x| *x < 10) {
+        println!("{}", i);
+    }
+
+    println!("Hello, infinite sequence!");
 }
